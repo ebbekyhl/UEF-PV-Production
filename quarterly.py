784fe4d9e16d978fc651946c9558ce7c334a611d
Ebbe Kyhl Gøtske <ebbekg@hotmail.com>
2025-09-30 14:05:05 +0100

    swapping dates of action

--- quarterly.py
+++ quarterly.py
@@ -42,32 +42,6 @@ print(year_months_downloads)
 for year_month_download in year_months_downloads:
     download.download_pv_data(year_month_download,download_dir)
 
-# For comparison, we show the expected production from simulation:
-pvgis = {"Jan": 895.7,
-        "Feb": 2037.71,
-        "Mar": 5641.7,
-        "Apr": 9541.1,
-        "Maj": 12132.2,
-        "Jun": 12686.7,
-        "Jul": 11868.2,
-        "Aug": 9501.8,
-        "Sep": 6703.4,
-        "Okt": 3242.3,
-        "Nov": 1108.7,
-        "Dec": 562.9}
-
-# plotting configuration
-fs = 14
-plt.rcParams['axes.labelsize'] = fs
-plt.rcParams['axes.titlesize'] = fs + 2
-plt.rcParams['xtick.labelsize'] = fs
-plt.rcParams['ytick.labelsize'] = fs
-plt.rcParams['xtick.direction'] = 'out'
-plt.rcParams['ytick.direction'] = 'out'
-plt.rcParams['axes.axisbelow'] = True
-plt.rcParams['legend.title_fontsize'] = fs+3
-plt.rcParams['legend.fontsize'] = fs
-
 # read data for every months contained in "year_months"
 df = pd.concat([pd.read_csv(download_dir + f"/PV_production_Aarhus_{month}.csv", sep=";") for month in year_months])
 df.rename(columns={"Ep":"Produktion"}, inplace=True)
@@ -102,59 +76,3 @@ with open("email_summary.txt", "w", newline="") as f:
     f.write(f"Samlet produktion for {extract_month_name(year_months[-3])} {extract_year(year_months[-3])}: {format_number(production_three_months_ago)} kWh\r\n")
     f.write(f"Samlet produktion for {extract_month_name(year_months[-2])} {extract_year(year_months[-2])}: {format_number(production_two_months_ago)} kWh\r\n")
     f.write(f"Samlet produktion for {extract_month_name(year_months[-1])} {extract_year(year_months[-1])}: {format_number(production_last_month)} kWh\r\n")
-
-########################################################################################
-########################### Daily production plot ######################################
-########################################################################################
-
-fig, ax = plt.subplots(figsize=(8, 5))
-df.plot(ax=ax, legend=False, color="darkorange")
-
-ax.set_title("Daily production values")
-ax.set_ylabel("kWh")
-ax.set_xlabel("")
-ax.grid()
-
-# add a double-directed arrow just below the graph of the last month 
-arrowprops = dict(arrowstyle="<->", lw=2, color="gray")
-
-subtract = 1/len(year_months)  # calculate the width of the arrow based on the number of months
-y_mean = production_monthly_mean.iloc[-1].item()
-y_range = ax.get_ylim()[1] - ax.get_ylim()[0]
-y_text = 1 - y_mean/y_range
-x_text = (2 - subtract)/2 
-
-ax.annotate("", xy=(1.0, y_text), xytext=(1.0 - subtract, y_text), 
-            xycoords='axes fraction', textcoords='axes fraction',
-            arrowprops=arrowprops)
-
-text = production_monthly_sum.index[-1] + " " +  year_months[-1][0:4]
-
-ax.text(x_text, y_text - y_range*0.00001, text, ha='center', va='top', transform=ax.transAxes, fontsize=fs, color="k", alpha=0.75)
-
-# savefig
-fig.savefig("figures/production_" + year_months[-1] + "_daily.png")
-
-########################################################################################
-########################### Monthly production plot ####################################
-########################################################################################
-
-# initialize figure
-fig_m, ax_m = plt.subplots(figsize=(8, 5))
-
-# bar plot of monthly production
-production_monthly_sum.plot(kind="bar", ax=ax_m, color="darkorange", alpha=0.7, edgecolor="k", width=0.8)
-
-# add expected production from simulation
-pd.Series(pvgis).loc[production_monthly_sum.index].plot(marker="X", ls="--", color="k", alpha=0.6, label="Forventet", ax=ax_m)
-
-# figure formatting
-ax_m.grid()
-ax_m.set_title("Månedlige produktionsværdier (" + year_months[-1][0:4] + ")")
-ax_m.set_ylabel("kWh")
-ax_m.set_xlabel("")
-ax_m.set_xticklabels(ax_m.get_xticklabels(), rotation=0, ha='center')
-ax_m.legend()
-
-# savefig
-fig_m.savefig("figures/production_" + year_months[-1] + "_monthly.png")
